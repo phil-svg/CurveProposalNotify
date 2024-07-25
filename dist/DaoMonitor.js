b33670e9d815252071091ddc184029c7152b1f62
phil <phil.softwaredev@protonmail.com>
2024-07-25 17:00:01 +0200

    prod

--- dist/DaoMonitor.js
+++ dist/DaoMonitor.js
@@ -4,8 +4,8 @@ import { fetchLast25Proposal } from './utils/subgraph/Proposal.js';
 import { getNotifiedIds, getNotifiedIdsPassedVotes, storeNotifiedId, storeNotifiedIdPassedVotes, } from './utils/memory/storage.js';
 import { sleep } from './utils/helper.js';
 console.clear();
-// const ENV = 'prod';
-const ENV = 'test';
+const ENV = 'prod';
+// const ENV = 'test';
 const eventEmitter = new EventEmitter();
 async function fetchAndNotify_New_Votes() {
     const reversedProposals = await fetchLast25Proposal();
